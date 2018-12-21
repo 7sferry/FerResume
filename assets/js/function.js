8b7713aa233dacfaf4449b7d4fdcbadfcecd1365
MR Ferry <ferry.suhandri@pji-solutions.com>
2018-12-21 20:08:50 +0700

    add tagline animation add circle animation

--- assets/js/function.js
+++ assets/js/function.js
@@ -11,10 +11,52 @@ $(function() {
     autobio(7000);
     bio();
     AOS.init({
-        duration: 1500
-    })
+        duration: 750
+    });
+    initBubbles();
+    for (var i = 0; i < words.length; i++) {
+        splitLetters(words[i]);
+    }
+    setInterval(changeWord, 3000);
 });
 
+var bubbleLifeTime = 3;
+var noOfBubbles = 15;
+
+var wrapper = document.querySelector('h1');
+var bubbles = [];
+
+
+function initBubbles() {
+    var bubble;
+    for(var i = 0; i < noOfBubbles; i++) {
+        bubble = createBubble();
+        wrapper.appendChild(bubble);
+    }
+}
+
+function createBubble() {
+    var circleContainer = document.createElement('div');
+    circleContainer.classList.add('circle_container');
+    circleContainer.style.transform = "rotate(" + Math.floor(Math.random() * 360) + "deg)";
+
+    var circle = createCircle();
+    circleContainer.appendChild(circle);
+
+    return circleContainer;
+}
+
+function createCircle() {
+    var circle = document.createElement('div');
+    circle.classList.add('circle');
+    circle.style.animationDelay = (Math.random() * bubbleLifeTime) + 's';
+
+    var side = (20 + Math.floor(Math.random() * 5)) + 'px';
+    circle.style.width = side;
+    circle.style.height = side;
+
+    return circle;
+}
 
 function smoothScroll (duration) {
     $('a[href^="#"]').on('click', function(event) {
@@ -168,3 +210,54 @@ function Timer(fn, t) {
         return this.stop().start();
     }
 }
+
+var words = $('.word');
+var wordArray = [];
+var currentWord = 0;
+
+words[currentWord].style.opacity = 1;
+
+function changeWord() {
+    var cw = wordArray[currentWord];
+    var nw = currentWord == words.length-1 ? wordArray[0] : wordArray[currentWord+1];
+    for (var i = 0; i < cw.length; i++) {
+        animateLetterOut(cw, i);
+    }
+
+    for (var i = 0; i < nw.length; i++) {
+        nw[i].className = 'letter behind';
+        nw[0].parentElement.style.opacity = 1;
+        animateLetterIn(nw, i);
+    }
+
+    currentWord = (currentWord == wordArray.length-1) ? 0 : currentWord+1;
+}
+
+function animateLetterOut(cw, i) {
+    setTimeout(function() {
+        cw[i].className = 'letter out';
+    }, i*80);
+}
+
+function animateLetterIn(nw, i) {
+    setTimeout(function() {
+        nw[i].className = 'letter in';
+    }, 340+(i*80));
+}
+
+function splitLetters(word) {
+    var content = word.innerHTML;
+    word.innerHTML = '';
+    var letters = [];
+    for (var i = 0; i < content.length; i++) {
+        var letter = document.createElement('span');
+        letter.className = 'letter';
+        letter.innerHTML = content.charAt(i);
+        word.appendChild(letter);
+        letters.push(letter);
+    }
+
+    wordArray.push(letters);
+}
+
+changeWord();
