e1e68c8bf3dadebd0a91d1ef66f87c41690b11ae
MR Ferry <ferry.suhandri@pji-solutions.com>
2019-01-02 16:29:41 +0700

    add experiences in timeline

--- assets/js/function.js
+++ assets/js/function.js
@@ -18,6 +18,7 @@ $(function() {
         splitLetters(words[i]);
     }
     setInterval(changeWord, 3000);
+    swiping();
 });
 
 var bubbleLifeTime = 3;
@@ -261,3 +262,25 @@ function splitLetters(word) {
 }
 
 changeWord();
+
+function swiping() {
+
+    var timelineSwiper = new Swiper ('.timeline .swiper-container', {
+        direction: 'vertical',
+        loop: false,
+        speed: 1600,
+        pagination: '.swiper-pagination',
+        paginationBulletRender: function (swiper, index, className) {
+            var year = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-year');
+            return '<span class="' + className + '">' + year + '</span>';
+        },
+        paginationClickable: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        breakpoints: {
+            768: {
+                direction: 'horizontal',
+            }
+        }
+    });
+}
